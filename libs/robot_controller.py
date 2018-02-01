4eb5ba85cfb96c17aa3ccc9bf04d24d4a0919b8e
huh2 <huh2@rose-hulman.edu>
2018-02-01 08:46:54 -0500

    Hao

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -16,43 +16,50 @@ import ev3dev.ev3 as ev3
 
 
 
-left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
-right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
-assert left_motor.connected
-assert right_motor.connected
+
+
 
 class Snatch3r(object):
     'This is called by m3_drive_inches_via_library and uses its inputs to tell the robot how fast to go and how far using a class def'
     def drive_inches(self, distance, speed):
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+        assert self.left_motor.connected
+        assert self.right_motor.connected
         sp = speed
         deg = distance * 90
-        left_motor.run_to_rel_pos(position_sp=deg, speed_sp=sp, stop_action=ev3.Motor.STOP_ACTION_BRAKE)
-        right_motor.run_to_rel_pos(position_sp=deg, speed_sp=sp, stop_action=ev3.Motor.STOP_ACTION_BRAKE)
-        left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        self.left_motor.run_to_rel_pos(position_sp=deg, speed_sp=sp, stop_action=ev3.Motor.STOP_ACTION_BRAKE)
+        self.right_motor.run_to_rel_pos(position_sp=deg, speed_sp=sp, stop_action=ev3.Motor.STOP_ACTION_BRAKE)
+        self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
 
     def turn_degrees(self, degrees_to_turn, turn_speed_sp):
         ev3.Sound.speak("Timed turn").wait()
 
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+        assert self.left_motor.connected
+        assert self.right_motor.connected
+
         time_s = 1
         while time_s != 0:
             if degrees_to_turn > 0:
-                left_motor.run_to_rel_pos(speed_sp=turn_speed_sp,position_sp=-degrees_to_turn*4.3,
+                self.left_motor.run_to_rel_pos(speed_sp=turn_speed_sp,position_sp=-degrees_to_turn*4.3,
                                           stop_action=ev3.Motor.STOP_ACTION_BRAKE)
-                right_motor.run_to_rel_pos(speed_sp=turn_speed_sp,position_sp=degrees_to_turn*4.3,
+                self.right_motor.run_to_rel_pos(speed_sp=turn_speed_sp,position_sp=degrees_to_turn*4.3,
                                            stop_action=ev3.Motor.STOP_ACTION_BRAKE)
-                left_motor.wait_while(ev3.Motor.STATE_RUNNING)
-                right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+                self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+                self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
                 ev3.Sound.beep().wait()
                 time_s=0
 
 
             elif degrees_to_turn < 0:
-                left_motor.run_to_rel_pos(speed_sp=turn_speed_sp,position_sp=-degrees_to_turn*4.3,
+                self.left_motor.run_to_rel_pos(speed_sp=turn_speed_sp,position_sp=-degrees_to_turn*4.3,
                                           stop_action=ev3.Motor.STOP_ACTION_BRAKE)
-                right_motor.run_to_rel_pos(speed_sp=turn_speed_sp, position_sp=degrees_to_turn*4.3,
+                self.right_motor.run_to_rel_pos(speed_sp=turn_speed_sp, position_sp=degrees_to_turn*4.3,
                                            stop_action=ev3.Motor.STOP_ACTION_BRAKE)
-                left_motor.wait_while(ev3.Motor.STATE_RUNNING)
-                right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+                self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+                self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
                 ev3.Sound.beep().wait()
                 time_s=0
 
