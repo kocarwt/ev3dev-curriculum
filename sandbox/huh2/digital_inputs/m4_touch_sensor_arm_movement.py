0113fabc09c247d7fd9c742bfd1e87791b733af9
kocarwt <kocawt@rose-hulman.edu>
2018-02-05 08:28:05 -0500

    m2_digital_inputs working on

--- sandbox/huh2/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/huh2/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -75,14 +75,12 @@ def arm_calibration(arm_motor, touch_sensor):
     # Code that attempts to do this task but has MANY bugs (nearly 1 on every line).  Fix them!
     arm_motor.run_forever(speed_sp=900)
     while not touch_sensor:
+
         time.sleep(0.01)
-    while touch_sensor:
-        arm_motor.stop(stop_action=arm_motor.STOP_ACTION_BRAKE)
-
-    arm_revolutions_for_full_range = 14.2
-    arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range)
-    arm_motor.wait_while(ev3.Motor.STATE_STALLED)
-
+    arm_motor.stop(stop_action='brake')
+    arm_revolutions_for_full_range = 14.2*360
+    arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range, speed_sp=MAX_SPEED)
+    arm_motor.wait_while(ev3.Motor.STATE_RUNNING)
     arm_motor.position = 0  # Calibrate the down position as 0 (this line is correct as is).
 
 
